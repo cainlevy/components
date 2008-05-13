fbb3c03ed834ab08ae60990c67766d2bc3684bb8
Lance Ivy <lance@cainlevy.net>
2008-05-13 14:32:38 -0700

    documentation

--- lib/components.rb
+++ lib/components.rb
@@ -1,4 +1,4 @@
-module Components
+module Components #:nodoc:
   def self.render(name, component_args = [], options = {})
     klass, method = name.split('/')
     component = (klass + "_component").camelcase.constantize.new
@@ -7,12 +7,46 @@ module Components
   end
 
   module ActionController
+    # Renders the named component with the given arguments. The component name must indicate both
+    # the component class and the class' action.
+    #
+    # Example:
+    #
+    #   class UsersController < ApplicationController
+    #     def show
+    #       render :text => component("users/details", params[:id])
+    #     end
+    #   end
+    #
+    # would render:
+    #
+    #   class UsersComponent < Components::Base
+    #     def details(user_id)
+    #       "all the important details about the user, nicely marked up"
+    #     end
+    #   end
     def component(name, *args)
       Components.render(name, args, :form_authenticity_token => form_authenticity_token)
     end
   end
 
   module ActionView
+    # Renders the named component with the given arguments. The component name must indicate both
+    # the component class and the class' action.
+    #
+    # Example:
+    #
+    #   /app/views/users/show.html.erb
+    #
+    #     <%= component "users/details", @user.id %>
+    #
+    # would render:
+    #
+    #   class UsersComponent < Components::Base
+    #     def details(user_id)
+    #       "all the important details about the user, nicely marked up"
+    #     end
+    #   end
     def component(name, *args)
       Components.render(name, args, :form_authenticity_token => form_authenticity_token)
     end
--- lib/components/base.rb
+++ lib/components/base.rb
@@ -3,13 +3,16 @@ module Components
     include ::ActionController::UrlWriter
 
     # for request forgery protection compatibility
-    attr_accessor :form_authenticity_token
+    attr_accessor :form_authenticity_token #:nodoc:
     delegate :request_forgery_protection_token, :allow_forgery_protection, :to => "ActionController::Base"
-    def protect_against_forgery?
+    def protect_against_forgery? #:nodoc:
       allow_forgery_protection && request_forgery_protection_token
     end
 
     class << self
+      # The view paths to search for templates. Typically this will only be "app/components", but
+      # if you have a plugin that uses Components, it may add its own directory (e.g.
+      # "vendor/plugins/scaffolding/components/" to this array.
       def view_paths
         if read_inheritable_attribute(:view_paths).nil?
           write_inheritable_attribute(:view_paths, [File.join(RAILS_ROOT, 'app', 'components')])
@@ -17,18 +20,41 @@ module Components
         read_inheritable_attribute(:view_paths)
       end
 
-      def name
+      def name #:nodoc:
         @name ||= self.to_s.underscore.sub("_component", "")
       end
     end
 
     # must be public for access from ActionView
-    def logger
+    def logger #:nodoc:
       RAILS_DEFAULT_LOGGER
     end
 
     protected
 
+    # When the string your component must return is complex enough to warrant a template file,
+    # this will render that file and return the result. Any template engine (erb, haml, etc.)
+    # that ActionView is capable of using can be used for templating.
+    #
+    # All instance variables that you create in the component action will be available from
+    # the view. There is currently no other way to provide variables to the views.
+    #
+    # === Inferred Template Name
+    #
+    # If you call render without a file name, it will:
+    #  * assume that the name of the calling method is also the name of the template file
+    #  * search for the named template file in the directory of this component's views, then the directories of all parent components
+    #
+    # This means that if you have:
+    #
+    #   class UsersComponent < Components::Base
+    #     def details(user_id)
+    #       render
+    #     end
+    #   end
+    #
+    # Then render will essentially assume that you meant to render "users/details", which may
+    # be found at "app/components/users/details.erb".
     def render(file = nil)
       # infer the render file basename from the caller method.
       unless file
@@ -47,18 +73,22 @@ module Components
       template.render("#{component.name}/#{file}")
     end
 
-    def template
+    # creates and returns a view object for rendering the current action.
+    # note that this freezes knowledge of view_paths and assigns.
+    def template #:nodoc:
       @template ||= Components::View.new(self.class.view_paths, assigns_for_view, self)
     end
 
-    def assigns_for_view
+    # should return a hash of all instance variables to assign to the view
+    def assigns_for_view #:nodoc:
       @assigns_for_view ||= (instance_variables - unassignable_instance_variables).inject({}) do |hash, var|
         hash[var[1..-1]] = instance_variable_get(var)
         hash
       end
     end
 
-    def unassignable_instance_variables
+    # should name all of the instance variables used by Components::Base that should _not_ be accessible from the view.
+    def unassignable_instance_variables #:nodoc:
       %w(@component_name @template)
     end
   end
